4732fb74cda997feca12436981e43bd97875f4f4
sasuketorii <sasuketorii.business@gmail.com>
2025-08-09 19:54:04 +0900

    feat(jp_bank_account_minimal): カナ入力を全角化・支店名カナから支店番号を自動補完・連絡先フィールドを説明付きで再表示\n\nrefs: #none

--- addons/jp_bank_account_minimal/models/res_partner_bank.py
+++ addons/jp_bank_account_minimal/models/res_partner_bank.py
@@ -3,13 +3,24 @@ from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 import re
 
-KANA_PATTERN = r'^[ｱ-ﾝﾞﾟｧ-ｫｬｭｮｯｰ\s\-\(\)0-9A-Z]+$'
+# 全角カナのパターン（記号・スペース含む）
+ZENKAKU_KANA_PATTERN = r'^[ァ-ンヴァ-ヶー・（）\s]+$'
+
+# 半角→全角カナの簡易マップ
+HALF_TO_FULL_KANA = str.maketrans({
+  'ｱ':'ア','ｲ':'イ','ｳ':'ウ','ｴ':'エ','ｵ':'オ','ｶ':'カ','ｷ':'キ','ｸ':'ク','ｹ':'ケ','ｺ':'コ',
+  'ｻ':'サ','ｼ':'シ','ｽ':'ス','ｾ':'セ','ｿ':'ソ','ﾀ':'タ','ﾁ':'チ','ﾂ':'ツ','ﾃ':'テ','ﾄ':'ト',
+  'ﾅ':'ナ','ﾆ':'ニ','ﾇ':'ヌ','ﾈ':'ネ','ﾉ':'ノ','ﾊ':'ハ','ﾋ':'ヒ','ﾌ':'フ','ﾍ':'ヘ','ﾎ':'ホ',
+  'ﾏ':'マ','ﾐ':'ミ','ﾑ':'ム','ﾒ':'メ','ﾓ':'モ','ﾔ':'ヤ','ﾕ':'ユ','ﾖ':'ヨ','ﾗ':'ラ','ﾘ':'リ',
+  'ﾙ':'ル','ﾚ':'レ','ﾛ':'ロ','ﾜ':'ワ','ｦ':'ヲ','ﾝ':'ン','ｧ':'ァ','ｨ':'ィ','ｩ':'ゥ','ｪ':'ェ','ｫ':'ォ',
+  'ｯ':'ッ','ｬ':'ャ','ｭ':'ュ','ｮ':'ョ','ｰ':'ー','･':'・','(': '（', ')': '）', ' ':'　'
+})
 
 class ResPartnerBank(models.Model):
   _inherit = 'res.partner.bank'
 
   jpBranchCode = fields.Char('支店番号', size=3, required=True)
-  jpBranchName = fields.Char('支店名')
+  jpBranchName = fields.Char('支店名（カナ）')
   jpAccountType = fields.Selection([
     ('ordinary', '普通'),
     ('current', '当座'),
@@ -17,7 +28,7 @@ class ResPartnerBank(models.Model):
     ('fixed', '定期'),
     ('other', 'その他'),
   ], string='口座種類', required=True, default='ordinary')
-  jpAccountHolderKana = fields.Char('口座名義人（カナ）', required=True, help='半角カナで入力')
+  jpAccountHolderKana = fields.Char('口座名義人（カナ）', required=True, help='全角カナで入力')
 
   @api.constrains('jpBranchCode')
   def _check_branch_code(self):
@@ -37,21 +48,25 @@ class ResPartnerBank(models.Model):
       self.acc_number = self.acc_number.zfill(7)
 
   @api.onchange('jpAccountHolderKana')
-  def _onchange_kana(self):
+  def _onchange_kana_fullwidth(self):
     if self.jpAccountHolderKana:
-      # 全角カナ→半角カナ（簡易: 濁点/半濁点は既存に倣い省略可）
-      trans = str.maketrans({
-        'ア':'ｱ','イ':'ｲ','ウ':'ｳ','エ':'ｴ','オ':'ｵ','カ':'ｶ','キ':'ｷ','ク':'ｸ','ケ':'ｹ','コ':'ｺ',
-        'サ':'ｻ','シ':'ｼ','ス':'ｽ','セ':'ｾ','ソ':'ｿ','タ':'ﾀ','チ':'ﾁ','ツ':'ﾂ','テ':'ﾃ','ト':'ﾄ',
-        'ナ':'ﾅ','ニ':'ﾆ','ヌ':'ﾇ','ネ':'ﾈ','ノ':'ﾉ','ハ':'ﾊ','ヒ':'ﾋ','フ':'ﾌ','ヘ':'ﾍ','ホ':'ﾎ',
-        'マ':'ﾏ','ミ':'ﾐ','ム':'ﾑ','メ':'ﾒ','モ':'ﾓ','ヤ':'ﾔ','ユ':'ﾕ','ヨ':'ﾖ','ラ':'ﾗ','リ':'ﾘ',
-        'ル':'ﾙ','レ':'ﾚ','ロ':'ﾛ','ワ':'ﾜ','ヲ':'ｦ','ン':'ﾝ','ァ':'ｧ','ィ':'ｨ','ゥ':'ｩ','ェ':'ｪ','ォ':'ｫ',
-        'ッ':'ｯ','ャ':'ｬ','ュ':'ｭ','ョ':'ｮ','ー':'ｰ','・':'･','　':' '
-      })
-      self.jpAccountHolderKana = self.jpAccountHolderKana.translate(trans)
+      self.jpAccountHolderKana = self.jpAccountHolderKana.translate(HALF_TO_FULL_KANA)
+
+  @api.onchange('jpBranchName')
+  def _onchange_branch_kana_fullwidth(self):
+    if self.jpBranchName:
+      self.jpBranchName = self.jpBranchName.translate(HALF_TO_FULL_KANA)
+      # 銀行＋支店名（カナ）から支店番号を自動補完
+      if self.bank_id:
+        branch = self.env['jp.bank.branch'].search([
+          ('bank_id', '=', self.bank_id.id),
+          ('branch_name_kana', '=', self.jpBranchName)
+        ], limit=1)
+        if branch:
+          self.jpBranchCode = branch.branch_code
 
   @api.constrains('jpAccountHolderKana')
   def _check_kana(self):
     for rec in self:
-      if rec.jpAccountHolderKana and not re.fullmatch(KANA_PATTERN, rec.jpAccountHolderKana):
-        raise ValidationError(_('口座名義人（カナ）は半角カナで入力してください。')) 
+      if rec.jpAccountHolderKana and not re.fullmatch(ZENKAKU_KANA_PATTERN, rec.jpAccountHolderKana):
+        raise ValidationError(_('口座名義人（カナ）は全角カナで入力してください。')) 
